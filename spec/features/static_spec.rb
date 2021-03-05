45fd6d2558b72907b14d03e5d5781eebd13bb12c
eclecticcoding <chuck@eclecticsaddlebag.com>
2021-03-04 19:36:07 -0500

    Add rspec, capybara and setup homepage

---
+++ spec/features/static_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.feature "Statics", type: :feature do
+  describe "homepage" do
+    it "can be reached successfully" do
+      visit root_path
+
+      expect(page.status_code).to eq(200)
+    end
+  end
+end
